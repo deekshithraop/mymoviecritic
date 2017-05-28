c92ffd31a7a0b57f89e92fb26c1a719927e37a33
deekshithraop <dpolusa000@citymail.cuny.edu>
2017-05-28 11:38:57 -0400

    Update main.bundle.js

--- static/main.bundle.js
+++ static/main.bundle.js
@@ -7847,7 +7847,7 @@ var ProfileComponent = (function () {
         this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
         this.userlink = "https://mydjangoapp57.herokuapp.com/api/v1/user/" + this.currentUser.id + "/?format=json";
         this.criticlink = 'https://mydjangoapp57.herokuapp.com/api/v1/critic?format=json';
-        this.myuserlink = "https://mydjangoapp57.herokuapp.com/api/v1/usercritic";
+        this.myuserlink = "https://mydjangoapp57.herokuapp.com/api/v1/usercritic/";
         this.http = http;
         this.allcritics();
         this.userDetails();
